2e2ef7d3ba8a8e9c4c3baec786d1c8c643b0be7e
ahmad yusuf setiawan <waone.on.gadget@gmail.com>
2020-08-12 13:21:43 +0700

    add apllication router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import Register from './Register';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+ } from "react-router-dom";
+import Home from './Home';
+/*import { Button } from 'semantic-ui-react';*/
 import './App.css';
+//import { register } from './serviceWorker';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+<Router>
+     <Switch>
+       <Route exact path='/'>
+         <Home/>
+       </Route>
+       <Route path='/register'>
+         <Register/>
+       </Route>
+     </Switch>
+   </Router>
+ );
 }
 
 export default App;
